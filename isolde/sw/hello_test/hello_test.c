1c8647b10414c0265ff2e4a8d3f708731e146ca5
darotsr <darotsr@tsr.ro>
2025-03-18 17:08:26 +0200

    merged tmp/conv2d into dev/isolde

---
+++ isolde/sw/hello_test/hello_test.c
@@ -0,0 +1,20 @@
+/*
+ *
+ * Copyleft 2025 ISOLDE
+ *
+ */
+
+//#include <stdio.h>
+#include <bsp/tinyprintf.h>
+#include <bsp/simple_system_common.h>
+#include <bsp/simple_system_regs.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+    printf("***  \n");
+    printf("***  Hello World from ISOLDE!\n");
+    printf("***  \n");
+    return 0;
+}
