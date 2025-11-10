94f3801796d1fe800dc2470ed13b95b7e1aa3765
Daniel Grosu <daniel.grosu@continental-corporation.com>
2025-11-10 16:40:07 +0200

    Modif UART_CLOCK_FREQ to match sys clk

--- vendor/isolde-soc/rtl/io/aida_io_pkg.sv
+++ vendor/isolde-soc/rtl/io/aida_io_pkg.sv
@@ -12,7 +12,7 @@ package aida_io_pkg;
   parameter int unsigned UART_BAUD_RATE = 115_200;
 
 `else
-  parameter int unsigned UART_CLOCK_FREQ = 100_000_000;  // sys_clk_i value in Hz
+  parameter int unsigned UART_CLOCK_FREQ = 10_000_000;  // sys_clk_i value in Hz
   parameter int unsigned UART_BAUD_RATE = 115_200;
 
 `endif
